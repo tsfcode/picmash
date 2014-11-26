d286200b3e299632c8f13e38bbcc13d232190076
tsfcode <tommy.sf.karlssson@gmail.com>
2014-11-26 17:00:36 +0100

    removed some unwanted whitespaces

--- main.go
+++ main.go
@@ -11,18 +11,18 @@ func main() {
 
 	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request){
 		city := strings.SplitN(r.URL.Path, "/", 3)[2]
-		
+
 		data, err := query(city)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
-		}	
+		}
 
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		json.NewEncoder(w).Encode(data)
 	})
 
-	http.ListenAndServe(":8080", nil)	
+	http.ListenAndServe(":8080", nil)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
